61e923bf7eb89b656696fa9e359230d1ea448f95
Pierre <pie.vancin@gmail.com>
2021-02-22 22:27:50 -0300

    terminado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Button } from'./components/Button' //Importa o button dentro do componente App
 
+import './styles/global.css';
+
 function App() {
   return (
     <div>Hello world
-    <Button color="blue"/>
+    <p><Button color="blue">
+      Botão 1
+    </Button>
+    </p>
+    <Button color="green">
+      Botão 2
+    </Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react'; //Hook usado para mudar o estado, usando um contador
+
 // Definindo uma interface
 interface ButtonProps {
-    color:string;
+    color: string;
+    children: string;
 }
 
 export function Button(props: ButtonProps){ //A função deve receber as propriedades da interface como parâmetro
+    const [counter, setCounter] = useState(1)
+
+    function increment() { //Não podemos mudar o valor da const counter por causa do conceito de imutabilidade, temos que usar a função setCounter que cria um novo valor para o counter
+        setCounter(counter + 1);
+    }
     return (
-        <button type="button" style={{ backgroundColor: props.color }}>
-            Botão
-            <strong>Teste</strong>
+        <button type="button" style={{ backgroundColor: props.color }} onClick={increment}>
+            {props.children} <strong>{counter}</strong>
         </button>
     );
 }
